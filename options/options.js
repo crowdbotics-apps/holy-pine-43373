5b95b06c5f0d8d756a43f98d60209b5280fe18f2
Crowdbotics <team@crowdbotics.com>
2023-09-13 23:51:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-pine-43373",
+  url: "https://holy-pine-43373.botics.co",
+  api: "https://holy-pine-43373.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
